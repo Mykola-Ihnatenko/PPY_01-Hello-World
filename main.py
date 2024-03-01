20802e3b05ab9c3339ce381c79f730dcd0a72631
Mykola Ihnatenko <s28339@pjwstk.edu.pl>
2024-03-01 13:06:52 +0100

    Task 2

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 # Task 1
-print(input("Enter first name: ") + " " + input("Enter last name: ") + " " + input("Enter age: "))
+#print(input("Enter first name: ") + " " + input("Enter last name: ") + " " + input("Enter age: "))
 
-
+# Task 2
+fahrenheit = float(input("Enter temperature in fahrenheit: "))
+celsius = (fahrenheit - 32) * 5 / 9
+print("Temperature in celsius:", celsius)
